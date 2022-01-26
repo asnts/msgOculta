c23c440b4ff62f90f0461f486ad410df7f5a2aab
Annanda Santos <annandasants@gmail.com>
2022-01-26 15:25:12 -0300

    Base64 finalizada e rodando

--- script.js
+++ script.js
@@ -1,97 +1,99 @@
-var cripto = document.querySelector('#selecionar'); 
+var cripto = document.querySelector('#selecionar');
 var intervalo = document.querySelector(".intervalo");
 var incremento = document.querySelector("#incremento");
-var btn = document.querySelector ('.enviar');
+var btn = document.querySelector('.enviar');
 var base64 = document.getElementById("base");
 var cesar = document.getElementById("cdc")
-var codificar = document.querySelector("#codificar");
-var decodificar = document.querySelector("#decodificar");
-
 
 
 // ocultando intervalo se base64 for escolhida
 
-    intervalo.style.display = "none";
-    incremento.style.display = "none";
-cripto.addEventListener("change" , function () {
+intervalo.style.display = "none";
+incremento.style.display = "none";
+cripto.addEventListener("change", function () {
     intervalo.style.transition = "none";
-    if (cripto.value == 'cesar'){
+    if (cripto.value == 'cesar') {
         intervalo.style.display = "flex";
         incremento.style.display = "flex";
-    }else {
+    } else {
         intervalo.style.display = "none";
-        incremento.style.display = "none"; 
+        incremento.style.display = "none";
 
     }
 })
 
 // adicionando o valor do intervalo
 
-intervalo.addEventListener("change", function() {
+intervalo.addEventListener("change", function () {
 
-    incremento =  document.getElementById("#incremento").value;
+    incremento = document.getElementById("#incremento").value;
 
 });
 
- //alterando o nome do botã0
+//alterando o nome do botã0
+
+var codificar = document.querySelector("#codificar");
+var decodificar = document.querySelector("#decodificar");
 
 codificar.addEventListener("change", function () {
-  btn.innerHTML = "Codificar"
-   
- })
+    btn.innerHTML = "Codificar"
 
- decodificar.addEventListener("change", function () {
-   btn.innerHTML ="Decodificar"
+})
 
- })
+decodificar.addEventListener("change", function () {
+    btn.innerHTML = "Decodificar"
+
+})
 
 
 // codificar na  base64
 
-var saida = document.querySelector (".saida");
+var saida = document.querySelector(".saida");
 var mensagem = document.querySelector('#msg');
 
-    function codificarB64 () {
+function codificarB64() {
     var texto = mensagem.value;
-    return btoa (texto)      
-      
-    }
-  
-    function decodificarB64(){
-    var texto = mensagem.value;
-    return atob (texto)
-     
-    }
+    return btoa(texto)
 
 
-  // Submeter resposta
+}
+
+function decodificarB64() {
+    var texto = mensagem.value;
+    return atob(texto)
+
+}
+
+
+// Submeter resposta
 
 btn.addEventListener("click", function (e) {
     e.preventDefault();
- 
- if(cripto.value === "base64"){
-    if(btn.value === "Codificar"){
 
-        saida.innerText = codificarB64();
-        
-    }else{
-       
-        saida.innerText = decodificarB64();
+    console.log(codificar.checked)
+    if (cripto.value === "base64") {
+        if (codificar.checked === true) {
+            console.log(codificarB64())
+            saida.value += codificarB64();
+
+        } else {
+
+            saida.value = decodificarB64();
+        }
+
+    } else if (cripto.value === "cifraCesar") {
+        if (submitButton.value === "Codificar") {
+            saida.innerText = codificarCesar();
+        }
+
+
+
+    } else {
+
+        saida.innerText = decodificarCesar();
     }
-  
-  }else if(cripto.value === "cifraCesar"){
-     if (submitButton.value === "Codificar"){
-        saida.innerText = codificarCesar();
-     }
-        
-        
-        
-    }else{
-         
-         saida.innerText = decodificarCesar();
-     }
- })
- 
+})
+
 
 
 
