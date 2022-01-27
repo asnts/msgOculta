85dc5a00c9769a712745017ec37e1bb4ec218651
Annanda Santos <annandasants@gmail.com>
2022-01-27 00:38:28 -0300

    finalizando cifra de cesar

--- cifraCesar.js
+++ cifraCesar.js
@@ -1,27 +1,50 @@
-// const letras = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];    
-const texto = document.getElementById("msg");
-const code = document.querySelector("#codificar");
-const decode = document.querySelector("#decodificar");
-const intervalo = document.getElementById('intervalo');
+var cesar = document.querySelector ("#cdc")
+var code = document.querySelector("#codificar");
+var decode = document.querySelector("#decodificar");
+var incremento = document.querySelector("#incremento");
+var saida = document.querySelector(".saida");
 
 
+function codificarCesar(msg, incremento){
+  msg = msg.split("");
+  incremento= parseInt(incremento,10);
+  var resposta = "";
+  for (i = 0; i <msg.length; i++){
+      var charCode = msg[i].charCodeAt ();
+      if (charCode >= 65 && charCode <= 90){
+          resposta += String.fromCharCode(((charCode + incremento - 65) %26) +65);
+          
+      }else if (charCode >=97 && charCode <=122){
+          resposta += String.fromCharCode(((charCode + incremento -97)%26) +97);
+      }else {
+          resposta +=msg[i];
+      }
 
-function codificarCesar(){
-
+  }
+    return saida.innerText = resposta;
 }
 
 
 
 
+function decodificarCesar(msg, incremento){
+    msg = msg.split("");
+  incremento= parseInt(incremento,10);
+  var resposta = "";
+  for (i = 0; i <msg.length; i++){
+      var charCode = msg[i].charCodeAt ();
+      if (charCode >= 65 && charCode <= 90){
+          resposta += String.fromCharCode(((charCode + incremento - 90) %26) + 90);
+          
+      }else if (charCode >=97 && charCode <=122){
+          resposta += String.fromCharCode(((charCode + incremento -122) %26) +122);
+      }else {
+          resposta +=msg[i];
+      }
 
-
-
-function decodificarCesar(){
+  }
+    return saida.innerText = resposta;
 
 }
 
 
-    // dar volta no dicionario usando chardCodeArt
-
-// formula cifra de cesar = (codigo da letra + 7) % 26
-// ((codigoDaLetraAsc - codigoPrimeiraLetra + 7) % tamanhodoAlfabeto) +codigoLetra
